26e807148ab8477309e956ce5aa77d423f5ac9ed
IndecisiveFly <kwok2549kai@gmail.com>
2017-03-26 19:18:54 -0700

    pieceAt and reset tested

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,7 +4,6 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
-#include <cstdio>
  
 class PiezasTest : public ::testing::Test
 {
@@ -14,6 +13,89 @@ class PiezasTest : public ::testing::Test
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
+
+TEST(PiezasTest, standared_reset)
+{
+  Piezas piezas;
+  piezas.dropPiece(0);
+  piezas.dropPiece(3);
+  piezas.dropPiece(0);
+  piezas.dropPiece(2);
+  piezas.dropPiece(0);
+  piezas.dropPiece(2);
+  piezas.dropPiece(3);
+
+  piezas.reset();
+
+  int flag=0;
+  for (int i=0; i<4; i++)
+  {
+    for (int j=0; j<3; j++)
+    {
+      if (piezas.pieceAt(j,i)!=Blank)
+      {
+        flag++;
+      }
+    }
+  }
+
+  ASSERT_EQ(0,flag);
+}
+
+TEST(PiezasTest, reset_when_blank)
+{
+  Piezas piezas;
+  piezas.reset();
+
+  int flag=0;
+  for (int i=0; i<4; i++)
+  {
+    for (int j=0; j<3; j++)
+    {
+      if (piezas.pieceAt(j,i)!=Blank)
+      {
+        flag++;
+      }
+    }
+  }
+
+  ASSERT_EQ(0,flag);
+}
+
+TEST(PiezasTest, check_blank)
+{
+  Piezas piezas;
+  Piece check=piezas.pieceAt(0,0);
+
+  ASSERT_EQ(check,Blank);
+}
+
+TEST(PiezasTest, check_out_of_bounds)
+{
+  Piezas piezas;
+  Piece check=piezas.pieceAt(0,4);
+
+  ASSERT_EQ(check,Invalid);
+}
+
+TEST(PiezasTest, check_stacked_piece)
+{
+  Piezas piezas;
+  piezas.dropPiece(2);
+  piezas.dropPiece(2);
+  Piece check=piezas.pieceAt(1,2);
+
+  ASSERT_EQ(check,O);
+}
+
+TEST(PiezasTest, check_first_placed)
+{
+  Piezas piezas;
+  piezas.dropPiece(3);
+  Piece check=piezas.pieceAt(0,3);
+
+  ASSERT_EQ(check,X);
+}
 
 TEST(PiezasTest, place_into_invalid)
 {
