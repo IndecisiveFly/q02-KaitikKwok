38d84b65a791e07751ff23b7acae0d61426f8c04
IndecisiveFly <kwok2549kai@gmail.com>
2017-03-26 18:43:24 -0700

    constructor and dropPiece tested

--- Piezas.cpp
+++ Piezas.cpp
@@ -1,5 +1,6 @@
 #include "Piezas.h"
 #include <vector>
+#include <iostream>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
  * on the game "Connect Four" where pieces are placed in a column and 
@@ -13,20 +14,40 @@
  * [0,0][0,1][0,2][0,3]
  * So that a piece dropped in column 2 should take [0,2] and the next one
  * dropped in column 2 should take [1,2].
-**/
+ **/
 
 
 /**
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
-**/
-Piezas::Piezas();
+ **/
+Piezas::Piezas()
+{
+  std::vector<Piece> row0(4,Blank);
+  std::vector<Piece> row1(4,Blank);
+  std::vector<Piece> row2(4,Blank);
+
+  board.push_back(row0);
+  board.push_back(row1);
+  board.push_back(row2);
+
+  turn=X;
+}
 
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
-**/
-void Piezas::reset();
+ **/
+void Piezas::reset()
+{
+  for (int col=0; col<4; col++)
+  {
+    for (int row=0; row<3; row++)
+    {
+      board[row][col]=Blank;
+    }
+  }
+}
 
 /**
  * Places a piece of the current turn on the board, returns what
@@ -35,14 +56,47 @@ void Piezas::reset();
  * In that case, placePiece returns Piece Blank value 
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
-Piece Piezas::dropPiece(int column);
+ **/ 
+Piece Piezas::dropPiece(int column)
+{
+  Piece returnvalue=Invalid;
+  if (column<4 && column>=0)
+  {
+    int i;
+    for (i=0; i<3; i++)
+    {
+      if (board[i][column]==Blank) //row not empty
+      {
+        board[i][column]=turn;
+        returnvalue=turn;
+        break;
+      }
+    }
+    if (i>=3) //row is full
+    {
+      returnvalue=Blank;
+    }
+  }
+  if (turn==X) //toggle turn
+    turn=O;
+  else
+    turn=X;
+  return returnvalue;
+}
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
-Piece Piezas::pieceAt(int row, int column);
+ **/
+Piece Piezas::pieceAt(int row, int column)
+{
+  Piece at_cord=Invalid;
+  if (row<3 && row>=0 && column<4 && column>=0)
+  {
+    at_cord=board[row][column];
+  }
+  return at_cord;
+}
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
@@ -52,5 +106,8 @@ Piece Piezas::pieceAt(int row, int column);
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
-**/
-Piece Piezas::gameState();
+ **/
+Piece Piezas::gameState()
+{
+  return Invalid;
+}
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
+#include <cstdio>
  
 class PiezasTest : public ::testing::Test
 {
@@ -13,6 +14,59 @@ class PiezasTest : public ::testing::Test
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
+
+TEST(PiezasTest, place_into_invalid)
+{
+  Piezas piezas;
+  Piece check=piezas.dropPiece(4);
+
+  ASSERT_EQ(check,Invalid);
+}
+
+TEST(PiezasTest, place_into_overflow)
+{
+  Piezas piezas;
+  piezas.dropPiece(0);
+  piezas.dropPiece(0);
+  piezas.dropPiece(0);
+
+  Piece check=piezas.dropPiece(0);
+  ASSERT_EQ(check,Blank);
+}
+
+TEST(PiezasTest, place_stacking_piece)
+{
+  Piezas piezas;
+  piezas.dropPiece(0);
+  Piece check=piezas.dropPiece(0);
+
+  ASSERT_EQ(check,O);
+}
+
+TEST(PiezasTest, place_first_piece)
+{
+  Piezas piezas;
+  Piece check=piezas.dropPiece(0);
+  
+  ASSERT_EQ(check,X);
+}
+
+TEST(PiezasTest, construct_empty_board)
+{
+  Piezas piezas;
+  int flag=0;
+  for (int col=0; col<4; col++)
+  {
+    for (int row=0; row<3; row++)
+    {
+      if ((piezas.pieceAt(row,col))!=Blank)
+      {
+        flag++;
+      }
+    }
+  }
+  ASSERT_EQ(flag,0);
+}
 
 TEST(PiezasTest, sanityCheck)
 {
